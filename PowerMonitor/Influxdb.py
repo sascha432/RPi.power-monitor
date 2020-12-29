a2c286ee56f08561bc4fcbe72e0d90bf08d84240
sascha <sascha_lammers@gmx.de>
2020-12-29 11:01:04 -0800

    cleanup and some info

--- PowerMonitor/Influxdb.py
+++ PowerMonitor/Influxdb.py
@@ -2,13 +2,13 @@
 # Author: sascha_lammers@gmx.de
 #
 
+#
+# This is currently not working
+#
+
 from . import Tools
 from . import Idle
-import hashlib
 import json
-import copy
-import numpy as np
-import EventManager
 import sys
 import time
 try:
@@ -22,7 +22,6 @@ class Influxdb(Idle.Idle):
     def __init__(self):
         global AppConfig
         AppConfig = self._app_config
-
         self.influx_client = None
 
     def start(self):
@@ -31,12 +30,11 @@ class Influxdb(Idle.Idle):
             if AppConfig.influxdb.host:
                 self.influx_client = InfluxDBClient(AppConfig.influxdb.host, AppConfig.influxdb.port, AppConfig.influxdb.username, AppConfig.influxdb.password, AppConfig.influxdb.database)
 
-    def destroy(self):
-        self.debug(__name__, 'destroy')
-        # self._mqtt_thread_state['quit'] = True
+    # def destroy(self):
+    #     self.debug(__name__, 'destroy')
 
-    def init_vars(self):
-        self.debug(__name__, 'init_vars')
+    # def init_vars(self):
+    #     self.debug(__name__, 'init_vars')
 
     def influxdb_push_data(self, data):
         time_str = time.strftime('%Y-%m-%dT%H:%M:%SZ', time.localtime())
@@ -54,5 +52,12 @@ class Influxdb(Idle.Idle):
                     'power': power
                 }
             }
-            self.debug(__name__, json.dumps(influx_data))
-            self.influx_client.write_points(json.dumps(influx_data))
+            #
+            # !!!
+            # this is causing an exception. probably invalid parameters or something not setup correctly in influxdb
+            #
+            try:
+                self.influx_client.write_points(json.dumps(influx_data))
+            except Exception as e:
+                self.debug(__name__, json.dumps(influx_data))
+                self.error(__name__, 'influxdb exception: %s' % e)
